bf18ec4b03f7fbd7d1b634b4d18745bba92c37b6
JitenChapagain <jitenchapagain45@gmail.com>
2024-05-15 15:31:21 +0545

    final change...

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib import messages
+
+from .forms import TodoForm
+from .forms import Todo
 
 # Create your views here.
 def index(request):
-    return HttpResponse ('JitenArjun')
+    item_list = Todo.objects.order_by("date")
+    if request.method =="POST":
+        form=TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo')
+    form = TodoForm()
+
+    page = {
+        "forms": form,
+        "list": item_list,
+        "title":"TODO LIST"
+    }
+    return render (request, 'index.html',page)
+
+def remove(request, item_id):
+    item =Todo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request,"item removed !!!!!!")
+    return redirect('todo')
